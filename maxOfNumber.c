655cb11a08707ef168b52b4d3ea4f5fe036343bc
Niranjan V S <32013719+NiranjanVS@users.noreply.github.com>
2019-08-07 11:52:06 +0530

    Add files via upload

---
+++ maxOfNumber.c
@@ -0,0 +1,52 @@
+/*#include<stdio.h>
+int main()
+{
+    int a[15];
+    int i,n;
+    int max=-999;
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    printf("Enter the elements of the Array");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+}*/
+
+
+
+#include<stdio.h>
+int FindMax(int a[],int n)
+{
+    int max=-999;
+    int i;
+     for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    return max;
+}
+int main()
+{
+       int a[15];
+    int i,n;
+    int max=-999;
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    printf("Enter the elements of the Array");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max = FindMax(int a,int n);
+    printf("%d",max);
+    return 0;
+}
